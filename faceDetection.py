6d845e07e31c96c59707e5a7ec2f9855ffe57f59
tuna <tunacinsoy@gmail.com>
2023-12-06 23:21:21 +0100

    Applying threshold

--- faceDetection.py
+++ faceDetection.py
@@ -27,6 +27,18 @@ def detect_edges(image):
     
     return edge_image
 
+def apply_threshold(edge_image, threshold_value=50):
+    # Convert edge image to numpy array if it's not already
+    edge_array = np.array(edge_image)
+    
+    # Apply threshold
+    binary_image = np.where(edge_array > threshold_value, 255, 0).astype('uint8')
+
+    # Convert back to PIL image
+    binary_image = Image.fromarray(binary_image)
+    
+    return binary_image
+
 # Load your image
 image_path = "C:\\Users\\tcins\\vscode-workspace\\EIASR-FaceRecognition\\GeorgeBush\\train\\George_W_Bush_0006.jpg"
 image = Image.open(image_path)
@@ -34,17 +46,31 @@ image = Image.open(image_path)
 # Detect edges
 edge_detected_image = detect_edges(image)
 
-# Display the results
-plt.figure(figsize=(10, 5))
+# Apply threshold to the edge-detected image
+threshold_value = 30  # We might need to adjust this value based on your image
+binary_image = apply_threshold(edge_detected_image, threshold_value)
 
-plt.subplot(1, 2, 1)
-plt.imshow(image, cmap='gray')
+# Display the results
+plt.figure(figsize=(15, 7))  # Adjust the figure size as needed
+
+# Subplot for the original image
+plt.subplot(1, 3, 1)
+plt.imshow(image)
 plt.title('Original Image')
 plt.axis('off')
 
-plt.subplot(1, 2, 2)
+# Subplot for the edge-detected image
+plt.subplot(1, 3, 2)
 plt.imshow(edge_detected_image, cmap='gray')
 plt.title('Edge Detected Image')
 plt.axis('off')
 
+# Subplot for the binary image after thresholding
+plt.subplot(1, 3, 3)
+plt.imshow(binary_image, cmap='gray')
+plt.title('Binary Image After Thresholding')
+plt.axis('off')
+
+plt.tight_layout()  # Adjust subplots to fit into the figure area.
 plt.show()
+
