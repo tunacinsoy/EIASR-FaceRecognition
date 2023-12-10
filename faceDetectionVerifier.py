503d5cdeb7e7546a1575f1ba921eed21d7f4c041
tuna <tunacinsoy@gmail.com>
2023-12-10 17:38:58 +0100

    Folder structure for verified images

--- faceDetectionVerifier.py
+++ faceDetectionVerifier.py
@@ -1,6 +1,5 @@
 import cv2
 import os
-import numpy as np
 import shutil
 
 def is_image_64x64_grayscale(image_path):
@@ -14,21 +13,22 @@ def contains_only_face(image_path):
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     return len(faces) == 1
 
-def copy_to_folder(source, destination):
-    shutil.copy(source, destination)
-
 def process_images(input_directory, output_directory):
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+    for person_name in os.listdir(input_directory):
+        person_path = os.path.join(input_directory, person_name)
+        if os.path.isdir(person_path):
+            output_person_path = os.path.join(output_directory, person_name)
+            if not os.path.exists(output_person_path):
+                os.makedirs(output_person_path)
 
-    for filename in os.listdir(input_directory):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            image_path = os.path.join(input_directory, filename)
-            if is_image_64x64_grayscale(image_path) and contains_only_face(image_path):
-                print(f"Image {filename} is suitable.")
-                copy_to_folder(image_path, os.path.join(output_directory, filename))
-            else:
-                print(f"Image {filename} is not suitable.")
+            for filename in os.listdir(person_path):
+                if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    image_path = os.path.join(person_path, filename)
+                    if is_image_64x64_grayscale(image_path) and contains_only_face(image_path):
+                        print(f"Image {filename} of {person_name} is suitable.")
+                        shutil.copy(image_path, output_person_path)
+                    else:
+                        print(f"Image {filename} of {person_name} is not suitable.")
 
 # Replace these paths with your input and output directories
 input_directory = "C:\\Users\\tcins\\vscode-workspace\\EIASR-FaceRecognition\\outputs"
